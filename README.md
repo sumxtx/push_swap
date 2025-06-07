eb04d55fcff15f9df2c27c851383ade0c3b02211
sumxtx <137871537+sumxtx@users.noreply.github.com>
2025-06-07 20:37:23 +0200

    init input parser

---
+++ README.md
@@ -0,0 +1,9 @@
+- parse the input
+- check for errors, exit
+- convert the input to integer
+- check for errors, exit
+- check if the integers are already sorted
+- create list nodes with the integers as content and link them
+- if the list is lower than 5 (easy sort)
+- else do more advanced sorts
+
--- push_swap.c
+++ push_swap.c
@@ -1,1 +1,42 @@
-#include 
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "libftall.h"
+
+int putstr_fd(const char *s, int fd)
+{
+  int len = ft_strlen(s);
+  write(fd, s, len);
+}
+ int vector_len(const char **s)
+{
+  int len;
+  len = 0;
+  while(s[len] != NULL)
+  {
+    len++;
+  }
+  return len;
+}
+
+int main(int ac, char **av)
+{
+  char **splitted;
+
+  if (ac == 1) // only progr name
+    exit(EXIT_FAILURE);
+  if (ac == 2) // maybe a string "12 23 45"
+  {
+    splitted = ft_split(av[1],' ');
+    if(!splitted)
+      exit(EXIT_FAILURE);
+  }
+  if (splitted)
+    ac = vector_len((const char**)splitted);
+  while(ac)
+  {
+    ac--;
+    printf("%d", ft_atoi(splitted[ac]));
+  }
+  return(EXIT_SUCCESS);
+}
